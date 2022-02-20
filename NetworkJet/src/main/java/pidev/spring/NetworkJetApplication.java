dc11abf22e6bd825b93e5c58a008b9014231dfdd
Sabghine <sabrine.mokhtar@esprit.tn>
2022-02-20 15:43:19 +0100

    Project dependencies

---
+++ NetworkJet/src/main/java/pidev/spring/NetworkJetApplication.java
@@ -0,0 +1,13 @@
+package pidev.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkJetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkJetApplication.class, args);
+	}
+
+}
---
+++ NetworkJet/src/test/java/pidev/spring/NetworkJetApplicationTests.java
@@ -0,0 +1,13 @@
+package pidev.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkJetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
