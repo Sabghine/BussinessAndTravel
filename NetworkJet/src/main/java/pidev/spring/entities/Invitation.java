a5012eca2dd4bdcc70113f814e12781a1ebbda6e
ihebsaadaoui <iheb.saadaoui@esprit.tn>
2022-02-20 16:45:09 +0100

    Update Invitation.java

--- NetworkJet/src/main/java/pidev/spring/entities/Invitation.java
+++ NetworkJet/src/main/java/pidev/spring/entities/Invitation.java
@@ -1,5 +1,26 @@
 package pidev.spring.entities;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "Invitation")
 public class Invitation {
+	private long idInvitation;
+	private Date date_invitation;
+	private Date date_expiration;
+	private String message;
+	private String token;
 
 }
