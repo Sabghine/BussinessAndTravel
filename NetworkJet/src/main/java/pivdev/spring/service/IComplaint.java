2fc486e043da25a60774668652def0ecd078a829
Sabghine <sabrine.mokhtar@esprit.tn>
2022-02-22 17:52:27 +0100

    Implemented IComplaint.java

---
+++ NetworkJet/src/main/java/pivdev/spring/service/IComplaint.java
@@ -0,0 +1,23 @@
+package pivdev.spring.service;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import pidev.spring.entities.Complaint;
+
+
+public interface IComplaint {
+	
+	List<Complaint> retrieveAllComplaints();
+
+	Complaint addFournisseur(Complaint c);
+
+	@Transactional
+	void deleteComplaint(Long complaintId);
+
+	Complaint updateComplaint(Complaint c);
+
+	Complaint retrieveComplaint(Long complaintId);
+
+}
