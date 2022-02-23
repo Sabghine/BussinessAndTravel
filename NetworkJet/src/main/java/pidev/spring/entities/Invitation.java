033f05bca74593b6b608e08e4022a4080d7301d8
ihebsaadaoui <iheb.saadaoui@esprit.tn>
2022-02-23 11:36:18 +0100

    Création des entités

--- NetworkJet/src/main/java/pidev/spring/entities/Invitation.java
+++ NetworkJet/src/main/java/pidev/spring/entities/Invitation.java
@@ -6,12 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
--- NetworkJet/src/main/java/pidev/spring/entities/Planning.java
+++ NetworkJet/src/main/java/pidev/spring/entities/Planning.java
@@ -1,5 +1,21 @@
 package pidev.spring.entities;
 
-public class Planning {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "Planning")
+public class Planning {
+	private long idPlanning;
+	private int nb_jours;
+	private String idVol;
 }
